a9641565420560b5efff2c4498da2068e2252d93
Tim Sheerman-Chase <tim2009@sheerman-chase.org.uk>
2016-09-29 22:43:07 +0100

    Reorganize before further work

--- dump.cpp
+++ dump.cpp
@@ -10,48 +10,8 @@
 #include <time.h>
 using namespace std;
 
-int main(int argc, char **argv)
-{	
-	std::filebuf outfi;
-	outfi.open("dump.o5m.gz", std::ios::out);
-	EncodeGzip *gzipEnc = new class EncodeGzip(outfi);
-
-	O5mEncode enc(*gzipEnc);
-	enc.StoreIsDiff(false);
-
-	string configContent;
-	cout << "Reading settings from config.cfg" << endl;
-	ReadFileContents("config.cfg", false, configContent);
-	std::vector<std::string> lines = split(configContent, '\n');
-	std::map<string, string> config;
-	for(size_t i=0; i < lines.size(); i++)
-	{
-		const std::string &line = lines[i];
-		std::vector<std::string> parts = split(line, ':');
-		if (parts.size() < 2) continue;
-		config[parts[0]] = parts[1];
-	}
-	
-	std::stringstream ss;
-	ss << "dbname=";
-	ss << config["dbname"];
-	ss << " user=";
-	ss << config["dbuser"];
-	ss << " password='";
-	ss << config["dbpass"];
-	ss << "' hostaddr=";
-	ss << config["dbhost"];
-	ss << " port=";
-	ss << "5432";
-	
-	pqxx::connection dbconn(ss.str());
-	if (dbconn.is_open()) {
-		cout << "Opened database successfully: " << dbconn.dbname() << endl;
-	} else {
-		cout << "Can't open database" << endl;
-		return 1;
-	}
-	
+void DumpNodes(pqxx::connection &dbconn, std::map<string, string> &config, O5mEncode &enc)
+{
 	cout << "Dump nodes" << endl;
 	stringstream sql;
 	sql << "SELECT *, ST_X(geom) as lon, ST_Y(geom) AS lat FROM ";
@@ -126,9 +86,9 @@ int main(int argc, char **argv)
 				cout << count << " nodes" << endl;
 
 			double timeNow = (double)clock() / CLOCKS_PER_SEC;
-			if (timeNow - lastUpdateTime > 1.0)
+			if (timeNow - lastUpdateTime > 30.0)
 			{
-				cout << count - lastUpdateCount << " nodes/sec" << endl;
+				cout << (count - lastUpdateCount)/30.0 << " nodes/sec" << endl;
 				lastUpdateCount = count;
 				lastUpdateTime = timeNow;
 			}
@@ -136,9 +96,52 @@ int main(int argc, char **argv)
 			enc.StoreNode(objId, metaData, handler.tagMap, lat, lon);
 		}
 
-		if (count >= 3000000)
-			exit(0);
 	}
+}
+
+int main(int argc, char **argv)
+{	
+	std::filebuf outfi;
+	outfi.open("dump.o5m.gz", std::ios::out);
+	EncodeGzip *gzipEnc = new class EncodeGzip(outfi);
+
+	O5mEncode enc(*gzipEnc);
+	enc.StoreIsDiff(false);
+
+	string configContent;
+	cout << "Reading settings from config.cfg" << endl;
+	ReadFileContents("config.cfg", false, configContent);
+	std::vector<std::string> lines = split(configContent, '\n');
+	std::map<string, string> config;
+	for(size_t i=0; i < lines.size(); i++)
+	{
+		const std::string &line = lines[i];
+		std::vector<std::string> parts = split(line, ':');
+		if (parts.size() < 2) continue;
+		config[parts[0]] = parts[1];
+	}
+	
+	std::stringstream ss;
+	ss << "dbname=";
+	ss << config["dbname"];
+	ss << " user=";
+	ss << config["dbuser"];
+	ss << " password='";
+	ss << config["dbpass"];
+	ss << "' hostaddr=";
+	ss << config["dbhost"];
+	ss << " port=";
+	ss << "5432";
+	
+	pqxx::connection dbconn(ss.str());
+	if (dbconn.is_open()) {
+		cout << "Opened database successfully: " << dbconn.dbname() << endl;
+	} else {
+		cout << "Can't open database" << endl;
+		return 1;
+	}
+	
+	DumpNodes(dbconn, config, enc);
 
 	enc.Finish();
 	delete gzipEnc;
